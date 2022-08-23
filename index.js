3e9543cde6c251f95403a7147792c49c3bb15ca8
mrslvs <sekerka.miroslav@outlook.com>
2022-08-23 18:00:06 +0200

    add data return from scrapeArt function

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const puppeteer = require('puppeteer');
+
+const selector = '.card';
+
+async function scrapeArt(url){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, {waitUntil: 'domcontentloaded'});
+
+    const cards = await page.$$eval(selector, (nodes) => {
+        return nodes.map(node => {
+            const src = node.querySelector('img').src;
+            const species = node.querySelector('.species').textContent;
+
+            return {
+                src,
+                species,
+            }
+        })
+    })
+
+    browser.close();
+
+    return cards;
+}
+
+scrapeArt('https://learnwebcode.github.io/practice-requests/').then(data => {
+    console.log(data);
+})
