eabebc2ef7204783d723e5d89fb54a8e774eab2a
mrslvs <sekerka.miroslav@outlook.com>
2022-08-23 23:24:15 +0200

    include extractInfo in scrapeArthuntSite to return array of ArtItem objects

--- index.js
+++ index.js
@@ -5,45 +5,6 @@ const puppeteer = require('puppeteer');
 const selector = '.eael-gallery-grid-item';
 
 /**
- * @typedef ArtItem
- * @property {string} imageSource - Image URL
- * @property {string} author - Author's name
- * @property {string} name - Name of the art item
- * @property {string} type - Type of the art item
- * @property {float} height - Height of the art item
- * @property {float} width - Width of the art ttem
- */
-
-/**
- * 
- * @param {string} url - URL address of art items 
- * @returns {ArtItem[]}
- */
-async function scrapeArt(url){
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url, {waitUntil: 'domcontentloaded'});
-
-    const arts = await page.$$eval(selector, (nodes) => {
-        return nodes.map(node => {
-            const imageSource = node.querySelector('.gallery-item-thumbnail-wrap').querySelector('img').src;
-            const author = node.querySelector('.fg-item-title').textContent;
-            const infoString = node.querySelector('.fg-item-content').querySelector('p').textContent; // 'Dielo: FebruaryTyp: PrintRozmer: 29,7 x 42 cm'
-
-            return {
-                imageSource,
-                author,
-                infoString,
-            }
-        })
-    })
-
-    browser.close();
-
-    return arts;
-}
-
-/**
  * @typedef {Object} ArtItemInformation
  * @property {string} name - Name of the art item
  * @property {string} type - Type of the art item
@@ -70,8 +31,53 @@ function extractInfo(infoString){
     return {name,type,height,width,};
 }
 
-// scrapeArt('https://www.arthunt.sk/diela/').then(data => {
-//     console.log(data);
-// })
 
-console.log(extractInfo('Dielo: FebruaryTyp: PrintRozmer: 29,7 x 42 cm'));
+
+/**
+ * @typedef ArtItem
+ * @property {string} imageSource - Image URL
+ * @property {string} author - Author's name
+ * @property {string} name - Name of the art item
+ * @property {string} type - Type of the art item
+ * @property {float} height - Height of the art item
+ * @property {float} width - Width of the art ttem
+ */
+
+/**
+ * 
+ * @param {string} url - URL address of art items 
+ * @returns {ArtItem[]}
+ */
+async function scrapeArthuntSite(url){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, {waitUntil: 'domcontentloaded'});
+
+    const arts = await page.$$eval(selector, (nodes) => {
+        return nodes.map(node => {
+            const imageSource = node.querySelector('.gallery-item-thumbnail-wrap').querySelector('img').src;
+            const author = node.querySelector('.fg-item-title').textContent;
+            const infoString = node.querySelector('.fg-item-content').querySelector('p').textContent; // 'Dielo: FebruaryTyp: PrintRozmer: 29,7 x 42 cm'
+
+            return {
+                imageSource,
+                author,
+                infoString,
+            }
+        })
+    })
+
+    browser.close();
+    
+    const artItemArray = arts.map(art => {
+        const {imageSource, author, infoString} = art;
+        let tmp = extractInfo(infoString);
+        return {imageSource, author, ...tmp};
+    })
+
+    return artItemArray;
+}
+
+scrapeArthuntSite('https://www.arthunt.sk/diela/').then(data => {
+    console.log(data);
+})
