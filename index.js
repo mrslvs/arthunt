d367973c8be78554b90ecfb1a7ea776ed0dc4bbd
mrslvs <sekerka.miroslav@outlook.com>
2022-08-23 21:20:42 +0200

    add extractInfo function to parse unformatted string from website

--- index.js
+++ index.js
@@ -9,28 +9,16 @@ async function scrapeArt(url){
     const page = await browser.newPage();
     await page.goto(url, {waitUntil: 'domcontentloaded'});
 
-    // const cards = await page.$$eval(selector, (nodes) => {
-    //     return nodes.map(node => {
-    //         const src = node.querySelector('img').src;
-    //         const species = node.querySelector('.species').textContent;
-
-    //         return {
-    //             src,
-    //             species,
-    //         }
-    //     })
-    // })
-
     const arts = await page.$$eval(selector, (nodes) => {
         return nodes.map(node => {
             const imageSource = node.querySelector('.gallery-item-thumbnail-wrap').querySelector('img').src;
             const author = node.querySelector('.fg-item-title').textContent;
-            const info = node.querySelector('.fg-item-content').querySelector('p').textContent;
+            const infoString = node.querySelector('.fg-item-content').querySelector('p').textContent; // 'Dielo: FebruaryTyp: PrintRozmer: 29,7 x 42 cm'
 
             return {
                 imageSource,
                 author,
-                info,
+                infoString,
             }
         })
     })
@@ -40,6 +28,33 @@ async function scrapeArt(url){
     return arts;
 }
 
-scrapeArt('https://www.arthunt.sk/diela/').then(data => {
-    console.log(data);
-})
+ /**
+  * 
+  * @param {string} infoString unformatted string containing all of the basic information about an art item, e.g., "Dielo: FebruaryTyp: PrintRozmer: 29,7 x 42 cm"
+  * @returns {Object} 
+  */
+function extractInfo(infoString){
+    // 'Dielo: FebruaryTyp: PrintRozmer: 29,7 x 42 cm'
+    let name;
+    let type;
+    let dimensions;
+    let rest;
+
+    [name, rest] = infoString.replace('Dielo: ', '').split('Typ: ');
+    [type, rest] = rest.split('Rozmer: ');
+    let [height, width] = rest.replace(' cm', '').replace(',', '.').split(' x ');
+    
+    
+    height = parseFloat(height);
+    width = parseFloat(width);
+
+    dimensions = {height, width,};
+
+    return {name,type,dimensions,};
+}
+
+// scrapeArt('https://www.arthunt.sk/diela/').then(data => {
+//     console.log(data);
+// })
+
+console.log(extractInfo('Dielo: FebruaryTyp: PrintRozmer: 29,7 x 42 cm'));
