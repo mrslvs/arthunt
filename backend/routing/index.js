a5709a30693ae95efa6a9ccaa81921be5bd04b66
mrslvs <sekerka.miroslav@outlook.com>
2022-08-24 15:47:16 +0200

    add express & listen on port 8080

---
+++ backend/routing/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on http://localhost:${PORT}`)
+});
