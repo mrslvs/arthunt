1b2b85fcce9138e7555753644af031fe58cb84c6
mrslvs <sekerka.miroslav@outlook.com>
2022-08-24 16:03:30 +0200

    add handler for POST at /art

--- backend/routing/index.js
+++ backend/routing/index.js
@@ -4,6 +4,29 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.use(express.json()); // use middleware
+
 app.listen(PORT, ()=>{
     console.log(`Listening on http://localhost:${PORT}`)
 });
+
+app.post('/art', (req,res) => {
+    const {code} = req.body;
+
+    if(!code){
+        res.status(418).send({
+            message: 'Missing code'
+        })
+        return;
+    }
+
+    if(code === codeArt){
+        res.status(200).send({
+            message: 'You have found yourself an art! Congratulations!'
+        })
+    }else{
+        res.status(400).send({
+            message: 'Incorrect code'
+        })
+    }  
+})
