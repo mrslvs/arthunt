b72430c3c6d9aaf112503f5961e0f41cbafb484b
mrslvs <sekerka.miroslav@outlook.com>
2022-08-25 19:51:28 +0200

    add dotenv & use database environment variables

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,11 +1,12 @@
 const {Client} = require('pg');
+require('dotenv').config({path: '../.env'});
 
 const client = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "root",
-    database: "postgres"
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD
 });
 
 client.connect();
