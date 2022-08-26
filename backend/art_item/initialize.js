6e60e270383176425d3cbb47a61acc7a40bed255
mrslvs <sekerka.miroslav@outlook.com>
2022-08-26 02:38:32 +0200

    add persist feature

--- backend/art_item/initialize.js
+++ backend/art_item/initialize.js
@@ -1,6 +1,10 @@
 const {ArtItem, scrapeArthuntSite, downloadImage} = require('./index.js');
+const {client} = require('../database/index.js');
+require('dotenv').config({path: '../.env'});
 
 let artItemArray = [];
+const databaseStructure = `("name", author, "type", imageURL, image, height, width, searchPhrase, code, "found")`;
+client.connect();
 
 scrapeArthuntSite().then((data) => {
     data.forEach(art => {
@@ -16,8 +20,19 @@ scrapeArthuntSite().then((data) => {
             art.width, 
             art.code
         );
+
+        client.query(`INSERT INTO ${process.env.DATABASE_TABLE} ${databaseStructure} VALUES ${tmp.persistQueryValues()}`, (err, res) => {
+            if(!err){
+                console.log(`Persisted ArtItem`);
+            }else{
+                console.log(err.message);
+            }
+        })
+
         artItemArray.push(tmp);
     });
     
     // create instances of ArtItem & persist them
 })
+
+client.end();
--- backend/database/index.js
+++ backend/database/index.js
@@ -1,5 +1,5 @@
 const client = require('./client.js');
 
 module.exports = {
-    client: client
+    client
 };
