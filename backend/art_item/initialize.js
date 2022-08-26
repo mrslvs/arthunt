a9a5bbf7bffc3e30fe9b47443cd1ebbbbbb5096a
mrslvs <sekerka.miroslav@outlook.com>
2022-08-26 11:37:08 +0200

    refactor old persist code

--- backend/art_item/initialize.js
+++ backend/art_item/initialize.js
@@ -1,10 +1,8 @@
 const {ArtItem, scrapeArthuntSite, downloadImage} = require('./index.js');
-const {client} = require('../database/index.js');
+const {client, insert} = require('../database/index.js');
 require('dotenv').config({path: '../.env'});
 
 let artItemArray = [];
-const databaseStructure = `("name", author, "type", imageURL, image, height, width, searchPhrase, code, "found")`;
-client.connect();
 
 scrapeArthuntSite().then((data) => {
     data.forEach(art => {
@@ -21,18 +19,7 @@ scrapeArthuntSite().then((data) => {
             art.code
         );
 
-        client.query(`INSERT INTO ${process.env.DATABASE_TABLE} ${databaseStructure} VALUES ${tmp.persistQueryValues()}`, (err, res) => {
-            if(!err){
-                console.log(`Persisted ArtItem`);
-            }else{
-                console.log(err.message);
-            }
-        })
-
         artItemArray.push(tmp);
-    });
-    
-    // create instances of ArtItem & persist them
-})
 
-client.end();
+    });
+})
