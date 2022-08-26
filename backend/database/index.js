5f42022a786a125741470b4466ec766aabd185bd
mrslvs <sekerka.miroslav@outlook.com>
2022-08-26 23:54:47 +0200

    add exportPublicData to database index

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,6 +1,7 @@
-const {insert} = require('./pool.js');
+const {insert, exportPublicData} = require('./pool.js');
 // const insert = require('./connect.js');
 
 module.exports = {
-    insert
+    insert,
+    exportPublicData
 };
