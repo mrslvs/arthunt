ff3d6f6a11104bfd7b6c783e225946f44a1b1691
mrslvs <sekerka.miroslav@outlook.com>
2022-08-29 11:02:03 +0200

    add socket.io to handle client-server communicaiton

--- backend/app.js
+++ backend/app.js
@@ -1,37 +1,72 @@
-const express = require('express');
 require('dotenv').config({path: './.env'});
-const bodyParser = require('body-parser');
-const fs = require('fs');
-
-const app = express();
-
 const PORT = process.env.APP_PORT;
 
-app.use(express.json()); // parse response body
-app.use(bodyParser.urlencoded({extended: false})); // parse GET/POST body received from client
-app.use(express.static('../frontend/'))
 
-// use middleware
-app.use((req, res, next) => {
-    // request
-    // response
-    // next = has to be executed to allow request to travel on to another middleware
-    console.log("Someone visited page");
-    next();
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) =>{
+    res.sendFile('/home/mrslvs/projects/arthunt/frontend/index.html');
 })
 
-app.get('/test', (req,res) => {
-    console.log('get request received and handled');
-    const data = JSON.parse(fs.readFileSync('./public.json', 'utf8'));
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
-    res.json(data);
-    // res.status(200).send('<h1>hi<h1>');
-})
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
 
-app.post('/', (req,res) =>{
-    console.log('post request received and handled');
-    console.log(req.body);
-    res.status(201).send('created ;)');
-})
 
-app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
+// const bodyParser = require('body-parser');
+// const fs = require('fs');
+
+
+
+
+
+// ===================================================================
+// const wss = new websocket.Server({server: app});
+// const PORT = process.env.APP_PORT;
+
+// app.use(express.json()); // parse response JSON body
+// app.use(bodyParser.urlencoded({extended: false})); // parse GET/POST body received from client
+// app.use(express.static('../frontend/'))
+
+// wss.on('connection', function connection(ws) {
+//     console.log("a new client connected to WSS");
+//     ws.send('welcome new client!');
+    
+//     ws.on('message', function incoming(message){
+//         console.log('received: %s', message)
+//     })
+// })
+
+// // use middleware
+// app.use((req, res, next) => {
+//     console.log("Someone visited page");
+//     next();
+// })
+
+// // app.get('/test', (req,res) => {
+//     //     console.log('get request received and handled');
+//     //     const data = JSON.parse(fs.readFileSync('./public.json', 'utf8'));
+    
+//     //     res.json(data);
+// //     // res.status(200).send('<h1>hi<h1>');
+// // })
+
+// app.post('/', (req,res) =>{
+//     console.log('post request received and handled');
+//     console.log(req.body);
+//     res.status(201).send('created ;)');
+// })
+
+// app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
+// ===================================================================
